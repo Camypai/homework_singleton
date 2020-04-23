d8eff7e69951632ce4f1924ee0bca7f2fa8888d4
Илья <igorshkov.93@yandex.ru>
2020-04-23 13:50:58 +0300

    Complete lazy

--- Homework_Singleton/Program.cs
+++ Homework_Singleton/Program.cs
@@ -7,26 +7,48 @@ namespace Homework_Singleton
     {
         public static void Main(string[] args)
         {
-            var process1 = new Thread(() =>
+            var processLock1 = new Thread(() =>
             {
-                TestSingleton("Thread 1");
+                TestSingletonLock("Thread lock 1");
             });
             
-            var process2 = new Thread(() =>
+            var processLock2 = new Thread(() =>
             {
-                TestSingleton("Thread 2");
+                TestSingletonLock("Thread lock 2");
             });
             
-            process1.Start();
-            process2.Start();
+            var processLazy1 = new Thread(() =>
+            {
+                TestSingletonLazy("Thread lazy 1");
+            });
             
-            process1.Join();
-            process2.Join();
+            var processLazy2 = new Thread(() =>
+            {
+                TestSingletonLazy("Thread lazy 2");
+            });
+            
+            processLock1.Start();
+            processLock2.Start();
+            
+            processLock1.Join();
+            processLock2.Join();
+            
+            processLazy1.Start();
+            processLazy2.Start();
+            
+            processLazy1.Join();
+            processLazy2.Join();
         }
 
-        private static void TestSingleton(string value)
+        private static void TestSingletonLock(string value)
         {
             var singleton = SingletonLock.GetSingleton(value);
+            Console.WriteLine(singleton.Value);
+        }
+        
+        private static void TestSingletonLazy(string value)
+        {
+            var singleton = SingletonLazy.GetSingleton(value);
             Console.WriteLine(singleton.Value);
         }
     }
---
+++ Homework_Singleton/SingletonLazy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework_Singleton
+{
+    public class SingletonLazy
+    {
+        private SingletonLazy(){}
+        
+        private static readonly Lazy<SingletonLazy> _singleton = new Lazy<SingletonLazy>(() => new SingletonLazy()) ;
+        
+        private static readonly object Lock = new object();
+        
+        public string Value { get; private set; }
+
+        public static SingletonLazy GetSingleton(string value)
+        {
+            lock (Lock)
+            {
+                if (_singleton.IsValueCreated) return _singleton.Value;
+            }
+            
+            lock (Lock)
+            {
+                if (!_singleton.IsValueCreated)
+                {
+                    _singleton.Value.Value = value;
+                }
+            }
+
+            return _singleton.Value;
+        }
+    }
+}
