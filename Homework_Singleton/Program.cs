3de97dee6a658dfc3d66c9c94ab27a821ce84cb2
Илья <igorshkov.93@yandex.ru>
2020-04-23 13:31:37 +0300

    Complete lock

--- Homework_Singleton/Program.cs
+++ Homework_Singleton/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace Homework_Singleton
+﻿using System;
+using System.Threading;
+
+namespace Homework_Singleton
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var process1 = new Thread(() =>
+            {
+                TestSingleton("Thread 1");
+            });
+            
+            var process2 = new Thread(() =>
+            {
+                TestSingleton("Thread 2");
+            });
+            
+            process1.Start();
+            process2.Start();
+            
+            process1.Join();
+            process2.Join();
+        }
+
+        private static void TestSingleton(string value)
+        {
+            var singleton = SingletonLock.GetSingleton(value);
+            Console.WriteLine(singleton.Value);
         }
     }
 }
